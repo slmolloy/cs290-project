ba2335585c23a27a76bbf73d8247963b015c00b8
Scott Molloy <scott.l.molloy@gmail.com>
2015-07-21 16:00:05 -0700

    Initial commit

---
+++ controllers/static.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var router = express.Router()
+var path = require('path')
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../layouts', 'app.html'))
+})
+
+router.use('/', express.static(path.join(__dirname, '../assets')))
+
+router.use(express.static(path.join(__dirname, '../templates')))
+
+module.exports = router
---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var fs = require('fs')
+fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
+  require('./gulp/' + task)
+})
+
+var gulp = require('gulp')
+gulp.task('dev', ['watch:css', 'watch:js', 'dev:server'])
