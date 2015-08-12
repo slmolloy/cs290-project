38b214e505288df919570649cdaa6a7e896c0348
Scott Molloy <scott.l.molloy@gmail.com>
2015-08-11 17:24:08 -0700

    Changing server startup to set app port

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var logger = require('morgan')
 var websockets = require('./websockets')
 
 var app = express()
+app.set('port', (process.env.PORT || 3000))
 
 app.use(logger('dev'))
 app.use(require('./controllers'))
 
-var port = process.env.PORT || 3000
-var server = app.listen(port, function() {
-  console.log('Server listening on', port)
+var server = app.listen(app.get('port'), function() {
+  console.log('Server listening on', app.get('port'))
 })
 
 websockets.connect(server)
