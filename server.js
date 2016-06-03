470a1f2c861863f83b83b221167c7d14381e4c91
Scott Molloy <scott.l.molloy@gmail.com>
2016-06-03 01:41:15 -0700

    Cleanup redis and morgan dependencies

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
-var logger = require('morgan');
 var websockets = require('./websockets');
 
 var app = express();
-app.set('port', (process.env.PORT || 3002));
+app.set('port', (process.env.PORT || 3000));
 
-app.use(logger('dev'));
 app.use(require('./controllers'));
 
 var server = app.listen(app.get('port'), function() {
